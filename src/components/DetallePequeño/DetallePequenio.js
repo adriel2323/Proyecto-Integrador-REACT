44be8d1f161d7df3fdd47a13410240319a7a362c
Adriel23 <adrielcolombo23@gmail.com>
2022-06-22 01:32:50 -0300

    Faltaba el de Ultimo producto ser funcional

--- src/components/DetallePequeño/DetallePequenio.js
+++ src/components/DetallePequeño/DetallePequenio.js
@@ -3,27 +3,24 @@ import PrecioCompra from "../DetallePequeño/PrecioCompra/PrecioCompra";
 import ImagenCurso from './imagenes/scrum.jpg'
 import cursos  from './cursosDataBase.json'
 import "./DetallePequenio.css"
-let descripcion = cursos[0].descripcion_corta
-let precio = cursos[0].precio
 
-
-function DetallePequenio (){
+function DetallePequenio (props){
     return (
         <div>
             <div>
                 <div className="DP-contenedor">
                     <div className="DP-foto">
-                        <img className="DP-imagen" src={ ImagenCurso }></img>
+                        <img className="DP-imagen" src={ props.imagen }></img>
                     </div>
                     <div className="DP-contenedor-info">
                         <h1 className="DP-titulo">
-                            titulo del curso
+                            {props.titulo}
                         </h1>
                         <h3>
                             <b>Descripcion</b>
                         </h3>
                         <p>
-                            {descripcion}
+                            {props.descripcion}
                         </p>
                     </div>
                 </div>
